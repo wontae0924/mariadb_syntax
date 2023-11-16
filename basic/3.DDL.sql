04d9f0968e8820cd29fc6023b33eee5ea885cec0
wontae0924 <wontae0924@gmail.com>
2023-11-16 17:56:36 +0900

    DB 명령문(3일차)

--- basic/3.DDL.sql
+++ basic/3.DDL.sql
@@ -2,5 +2,22 @@
 -- INSERT : 테이블에 새로운 레코드를 추가
 -- INSERT INTO 테이블이름(필드이름1, 필드이름2 ,....) VALUES (데이터 값1, 데이터값2,)
 insert into author(id, name, email) values(1,'kim','abc@naver.com');
+
+-- SELECT : SELECT 필드이름 FROM 테이블 이름 [where 조건];
+-- 모든 필드 검색  SELECT * FROM 테이블 이름;
 select*from author;
+select*from author where id=1;
+select name,email from author where id=1;
+select*from author where id>1;
+select password from author where id>1 and name='abc';
+
+--  UPDATE : 테이블 레코드의 내용을 수정
+--  UPDATE 테이블 이름 SET 필드 이름1=데이터값1, 필드 이름2 =데이터값2, WHERE 필드 이름 =데이터 값
+--  만약 where 절을 생략하면 
+update author set email='hello1@test.com'where id = 2;
+
+-- DELETE : 데이터 삭제
+-- delete from 테이블이름 where 필드이름=데이터 값
+-- where 절을 생략하면, 해당 테이블에 저장된 모든 데이터가 삭제
+delete from post where author_id=5;
 
--- basic/4.DDL_pracitice.sql
+++ basic/4.DDL_pracitice.sql
@@ -9,12 +9,20 @@ insert into author(id, name, email,password,role) values(6,'lee5','abc5@naver.co
 
 -- post 데이터 5개 추가
 --  -> 2개는 저자가 있는 데이터
---  -> 2개는 저자가 비어있는 데이터 -> author_id에 NOT NULL 조건없다는 사실 확인
---  -> 1개는 저자가 author_id에 없는 데이터 추가 -> 에러 발생 확인위해
 insert into post(id, title, contents,author_id) values(1,'title','hello world',1);
 insert into post(id, title, contents,author_id) values(2,'title1','hello world1',2);
-
-insert into post(id, contents,author_id) values(3,'hello world3',3);
-insert into post(id, contents,author_id) values(4,'hello world4',4);
-
+--  -> 2개는 저자가 비어있는 데이터 -> author_id에 NOT NULL 조건없다는 사실 확인
+insert into post(id, title ,contents) values(3,'title2','hello world3');
+insert into post(id, title , contents) values(4,'title3','hello world4');
+--  -> 1개는 저자가 author_id에 없는 데이터 추가 -> 에러 발생 확인위해
 insert into post(id, title, contents,author_id) values(5,'title5','hello world5',999);
+
+-- 3. 실습
+-- author 데이터 중 id가 4인 데이터를 email을 abc@naver.com, name을 abc로 변경
+update author set email='abc@naver.com', name='abc' where id=4;
+-- post에 글쓴적이 없는 author 데이터 1개 삭제
+delete from post where id=3; --id는 post에 글쓴적이 없음
+-- post에 글쓴적이 있는 author 데이터 1개 삭제 -> 에러 -> 조치후 삭제
+delete from author where id=2; -- 삭제 할려고 하나 id=2는 post에 글쓴적이 있어 삭제못하는 에러 발생
+delete from post where author_id=2; -- 하여 그 글쓴 기록을 삭제 조치
+delete from author where id=2; -- 다시 삭제하면 이상 없음
